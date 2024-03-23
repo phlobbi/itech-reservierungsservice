d81d0eb9058393fe6b2443d89a8891a9f5a4fd3b
Philip Dausend <dev@philip-dausend.de>
2024-03-23 18:59:02 +0100

    Add Command for creating times

---
+++ src/Command/CreateTimesCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\AvailableTimesService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:create-times', description: 'Create times for the next 7 days'
+)]
+class CreateTimesCommand extends Command
+{
+
+    public function __construct(
+        private AvailableTimesService $availableTimesService
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->availableTimesService->createTimes();
+
+        $output->writeln('Times created');
+
+        return Command::SUCCESS;
+    }
+}
--- src/Service/AvailableTimesService.php
+++ src/Service/AvailableTimesService.php
@@ -2,13 +2,18 @@
 
 namespace App\Service;
 
+use App\Entity\RestaurantAvailableTime;
 use App\Repository\RestaurantAvailableTimeRepository;
+use App\Repository\RestaurantTableRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AvailableTimesService
 {
 
     public function __construct(
-        private RestaurantAvailableTimeRepository $restaurantAvailableTimeRepository
+        private RestaurantAvailableTimeRepository $restaurantAvailableTimeRepository,
+        private RestaurantTableRepository $restaurantTableRepository,
+        private EntityManagerInterface $entityManager
     )
     {
 
@@ -36,8 +41,45 @@ class AvailableTimesService
 
         sort($times);
 
-        return array_map(function ($time) {
+        $stringTimes = array_map(function ($time) {
             return $time->format('H:i');
         }, $times);
+
+        return array_values(array_unique($stringTimes));
+    }
+
+    /**
+     * Erstellt für die nächsten 7 Tage Reservierungszeiten von 10 bis 20 Uhr
+     * @return void
+     */
+    public function createTimes(): void
+    {
+        // Create times for the next 7 days
+        $date = new \DateTime('today');
+        $endDate = new \DateTime('today + 7 days');
+
+        while ($date < $endDate) {
+            $timesAlreadySet = $this->restaurantAvailableTimeRepository->getAvailableTimesForOneDay($date);
+            if ($timesAlreadySet != null) {
+                $date->modify('+1 day');
+                continue;
+            }
+
+            $tables = $this->restaurantTableRepository->findAll();
+
+            foreach ($tables as $table) {
+                $time = new \DateTime( "10:00:00");
+                for ($i = 0; $i < 6; $i++) {
+                    $rat = new RestaurantAvailableTime();
+                    $rat->setDate($date);
+                    $rat->setTime(clone $time);
+                    $rat->setRestaurantTable($table);
+
+                    $this->entityManager->persist($rat);
+                    $time->modify('+2 hours');
+                }
+                $this->entityManager->flush();
+            }
+        }
     }
 }
